4ee70eca13ef08ed9931632d645682e6a7fa895a
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-05-12 15:40:30 +0200

    implement post scan config client method

--- integration_test/abort_scan_test.go
+++ integration_test/abort_scan_test.go
@@ -3,10 +3,24 @@ package integration_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/openclarity/vmclarity/api/models"
 )
 
-var _ = Describe("Dummy", func() {
-	It("tests if true is true", func() {
-		Expect(true).To(Equal(true))
+var _ = Describe("Aborting a scan", func() {
+	scanConfig := models.ScanConfig{
+		Disabled:                      nil,
+		Id:                            nil,
+		MaxParallelScanners:           nil,
+		Name:                          nil,
+		ScanFamiliesConfig:            nil,
+		ScannerInstanceCreationConfig: nil,
+		Scheduled:                     nil,
+		Scope:                         nil,
+	}
+	Context("which is running", func() {
+		It("should stop successfully", func(ctx SpecContext) {
+			resp, err := client.PostScanConfig(ctx, scanConfig)
+			Expect(true).To(Equal(true))
+		})
 	})
 })
