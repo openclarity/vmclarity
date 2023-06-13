994729c77691baf6028bdf095ee6ec2027e63338
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2023-06-13 17:21:13 +0200

    refactor: provider config

--- runtime_scan/pkg/provider/aws/config.go
+++ runtime_scan/pkg/provider/aws/config.go
@@ -15,40 +15,89 @@
 
 package aws
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 const (
-	AWSSubnetID            = "AWS_SUBNET_ID"
-	AWSJobImageID          = "AWS_JOB_IMAGE_ID"
-	AWSSecurityGroupID     = "AWS_SECURITY_GROUP_ID"
-	AWSInstanceType        = "AWS_INSTANCE_TYPE"
-	defaultAWSJobImageID   = "ami-0568773882d492fc8" // ubuntu server 22.04 LTS (HVM), SSD volume type
-	defaultAWSInstanceType = "t2.large"
+	EnvScannerRegion       = "VMCLARITY_AWS_SCANNER_REGION"
+	EnvSubnetID            = "VMCLARITY_AWS_SUBNET_ID"
+	EnvSecurityGroupID     = "VMCLARITY_AWS_SECURITY_GROUP_ID"
+	EnvKeyPairName         = "VMCLARITY_AWS_KEYPAIR_NAME"
+	EnvScannerImage        = "VMCLARITY_AWS_SCANNER_AMI_ID"
+	EnvScannerInstanceType = "VMCLARITY_AWS_SCANNER_INSTANCE_TYPE"
+	EnvBlockDeviceName     = "VMCLARITY_AWS_BLOCK_DEVICE_NAME"
+
+	DefaultScannerInstanceType = "t2.large"
+	DefaultBlockDeviceName     = "xvdh"
 )
 
 type Config struct {
-	AmiID           string // image id of a scanner job
-	SubnetID        string // the scanner's subnet ID
-	SecurityGroupID string // the scanner's security group
-	InstanceType    string // the scanner's instance type
+	// Region where the Scanner instance needs to be created
+	ScannerRegion string
+	// SubnetID where the Scanner instance needs to be created
+	SubnetID string
+	// SecurityGroupID which needs to be attached to the Scanner instance
+	SecurityGroupID string
+	// KeyPairName is the name of the SSH KeyPair to use for Scanner instance launch
+	KeyPairName string
+	// ScannerImage is the AMI image used for creating Scanner instance
+	ScannerImage string
+	// ScannerInstanceType is the instance type used for Scanner instance
+	ScannerInstanceType string
+	// BlockDeviceName contains the block device name used for attaching Scanner volume to the Scanner instance
+	BlockDeviceName string
 }
 
-func setConfigDefaults() {
-	viper.SetDefault(AWSJobImageID, defaultAWSJobImageID)
-	viper.SetDefault(AWSInstanceType, defaultAWSInstanceType)
-
-	viper.AutomaticEnv()
-}
-
-func LoadConfig() *Config {
-	setConfigDefaults()
-
-	config := &Config{
-		AmiID:           viper.GetString(AWSJobImageID),
-		SubnetID:        viper.GetString(AWSSubnetID),
-		SecurityGroupID: viper.GetString(AWSSecurityGroupID),
-		InstanceType:    viper.GetString(AWSInstanceType),
+func (c *Config) Validate() error {
+	if c.ScannerRegion == "" {
+		return fmt.Errorf("parameter Region must not be nil")
 	}
 
-	return config
+	if c.SubnetID == "" {
+		return fmt.Errorf("parameter SubnetID must not be nil")
+	}
+
+	if c.SecurityGroupID == "" {
+		return fmt.Errorf("parameter SecurityGroupID must not be nil")
+	}
+
+	if c.ScannerImage == "" {
+		return fmt.Errorf("parameter ScannerImage must not be nil")
+	}
+
+	if c.ScannerInstanceType == "" {
+		return fmt.Errorf("parameter ScannerInstanceType must not be nil")
+	}
+
+	return nil
+}
+
+func NewConfig() (*Config, error) {
+	// Avoid modifying the global instance
+	v := viper.New()
+
+	v.AllowEmptyEnv(true)
+	v.AutomaticEnv()
+
+	_ = v.BindEnv("ScannerRegion", EnvScannerRegion)
+	_ = v.BindEnv("SubnetID", EnvSubnetID)
+	_ = v.BindEnv("SecurityGroupID", EnvSecurityGroupID)
+	_ = v.BindEnv("KeyPairName", EnvKeyPairName)
+	_ = v.BindEnv("ScannerImage", EnvScannerImage)
+
+	_ = v.BindEnv("ScannerInstanceType", EnvScannerInstanceType)
+	v.SetDefault("ScannerInstanceType", DefaultScannerInstanceType)
+
+	_ = v.BindEnv("BlockDeviceName", EnvBlockDeviceName)
+	v.SetDefault("BlockDeviceName", DefaultBlockDeviceName)
+
+	config := &Config{}
+	if err := v.Unmarshal(config); err != nil {
+		return nil, fmt.Errorf("failed to parse provider configuration. Provider=AWS: %w", err)
+	}
+
+	return config, nil
 }
