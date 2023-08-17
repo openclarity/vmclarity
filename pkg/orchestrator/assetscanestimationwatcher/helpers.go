0d1656187dd98b37be9c6bc717eed6f4a66f9f88
Erez Fishhimer <erezf@cisco.com>
2023-08-17 17:45:21 +0300

    fix getLatestAssetScanStats

--- pkg/orchestrator/assetscanestimationwatcher/helpers.go
+++ pkg/orchestrator/assetscanestimationwatcher/helpers.go
@@ -13,7 +13,7 @@ import (
 func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Asset) (models.AssetScanStats, error) {
 	var stats models.AssetScanStats
 
-	filterTmpl := "asset/id eq %s and status/general/state eq 'Done' and status/general/errors eq null and scanFamiliesConfig/%s/enabled eq true"
+	filterTmpl := "asset/id eq '%s' and status/general/state eq 'Done' and status/general/errors eq null and scanFamiliesConfig/%s/enabled eq true"
 	// get the latest asset scan with exploits enabled
 	params := models.GetAssetScansParams{
 		Filter:  utils.PointerTo(fmt.Sprintf(filterTmpl, *asset.Id, "exploits")),
@@ -24,7 +24,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for exploits. Ommiting stats: %v", err)
 	} else {
-		stats.Exploits = (*res.Items)[0].Stats.Exploits
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Exploits = (*res.Items)[0].Stats.Exploits
+		}
 	}
 
 	// get the latest asset scan with sbom enabled
@@ -33,7 +35,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for sbom. Ommiting stats: %v", err)
 	} else {
-		stats.Sbom = (*res.Items)[0].Stats.Sbom
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Sbom = (*res.Items)[0].Stats.Sbom
+		}
 	}
 
 	// get the latest asset scan with vulnerability enabled
@@ -42,7 +46,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for vulnerabilities. Ommiting stats: %v", err)
 	} else {
-		stats.Vulnerabilities = (*res.Items)[0].Stats.Vulnerabilities
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Vulnerabilities = (*res.Items)[0].Stats.Vulnerabilities
+		}
 	}
 
 	// get the latest asset scan with malware enabled
@@ -51,7 +57,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for malware. Ommiting stats: %v", err)
 	} else {
-		stats.Malware = (*res.Items)[0].Stats.Malware
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Malware = (*res.Items)[0].Stats.Malware
+		}
 	}
 
 	// get the latest asset scan with misconfiguration enabled
@@ -60,7 +68,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for misconfigurations. Ommiting stats: %v", err)
 	} else {
-		stats.Misconfigurations = (*res.Items)[0].Stats.Misconfigurations
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Misconfigurations = (*res.Items)[0].Stats.Misconfigurations
+		}
 	}
 
 	// get the latest asset scan with rootkits enabled
@@ -69,7 +79,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for rootkits. Ommiting stats: %v", err)
 	} else {
-		stats.Rootkits = (*res.Items)[0].Stats.Rootkits
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Rootkits = (*res.Items)[0].Stats.Rootkits
+		}
 	}
 
 	// get the latest asset scan with secrets enabled
@@ -78,7 +90,9 @@ func (w *Watcher) getLatestAssetScanStats(ctx context.Context, asset *models.Ass
 	if err != nil {
 		logrus.Errorf("Failed to get asset scans for secrets. Ommiting stats: %v", err)
 	} else {
-		stats.Secrets = (*res.Items)[0].Stats.Secrets
+		if res.Items != nil && len(*res.Items) > 0 && (*res.Items)[0].Stats != nil {
+			stats.Secrets = (*res.Items)[0].Stats.Secrets
+		}
 	}
 
 	return stats, nil
