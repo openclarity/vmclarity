9797030856926e3cd1c3e5c16801b6f84ffbfd72
Tamás Galló <tamasg@cisco.com>
2023-10-30 16:01:13 +0100

    refactor(ui): destructure params inline

--- ui/src/layout/detail-displays/AssetDetails/CommonAssetMetadata.js
+++ ui/src/layout/detail-displays/AssetDetails/CommonAssetMetadata.js
@@ -3,20 +3,14 @@ import TitleValueDisplay, { TitleValueDisplayRow } from 'components/TitleValueDi
 import { formatDate } from 'utils/utils';
 
 
-export const CommonAssetMetadata = ({assetData}) => {
-    const {firstSeen, lastSeen, terminatedOn} = assetData;
-
-    return (
-        <>
-            <TitleValueDisplayRow>
-                <TitleValueDisplay title="Type">{assetData.assetInfo.objectType}</TitleValueDisplay>
-                <TitleValueDisplay title="First Seen">{formatDate(firstSeen)}</TitleValueDisplay>
-            </TitleValueDisplayRow>
-            <TitleValueDisplayRow>
-                
-                <TitleValueDisplay title="Last Seen">{formatDate(lastSeen)}</TitleValueDisplay>
-                <TitleValueDisplay title="Terminated On">{formatDate(terminatedOn)}</TitleValueDisplay>
-            </TitleValueDisplayRow>
-        </>
-    )
-}
+export const CommonAssetMetadata = ({assetData: {firstSeen, lastSeen, terminatedOn, assetInfo}}) =>
+    <>
+        <TitleValueDisplayRow>
+            <TitleValueDisplay title="Type">{assetInfo.objectType}</TitleValueDisplay>
+            <TitleValueDisplay title="First Seen">{formatDate(firstSeen)}</TitleValueDisplay>
+        </TitleValueDisplayRow>
+        <TitleValueDisplayRow>
+            <TitleValueDisplay title="Last Seen">{formatDate(lastSeen)}</TitleValueDisplay>
+            <TitleValueDisplay title="Terminated On">{formatDate(terminatedOn)}</TitleValueDisplay>
+        </TitleValueDisplayRow>
+    </>
