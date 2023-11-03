c098085a9244da4030005913fac3f1f48268e0a3
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-11-03 11:43:13 +0100

    fix(scans-count): completed scans count

--- ui/src/layout/Dashboard/CounterDisplay/index.js
+++ ui/src/layout/Dashboard/CounterDisplay/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import { useFetch } from 'hooks';
 import { formatNumber } from 'utils/utils';
 
 import './counter-display.scss';
 
 const CounterDisplay = ({url, title, background}) => {
-    const [{data, error, loading}] = useFetch(url, {queryParams: {"$count": true, "$top": 1, "$select": "id"}});
-    
+    const [{data, error, loading}] = useFetch(url, {queryParams: {"$select": "state"}});
+
+    const completedScans = useMemo(() => {
+        let ret = 0;
+
+        data?.items.forEach((element) => {
+            if (element.state === "Aborted" || element.state === "Failed" || element.state === "Done") {
+                ret++
+            }
+        })
+
+        return ret
+    }, [data])
+
     return (
         <div className="dashboard-counter" style={{background}}>
-            {loading || error ? "" : 
-                <div className="dashboard-counter-content"><div className="dashboard-counter-count">{formatNumber(data.count)}</div>{` ${title}`}</div>
+            {loading || error ? "" :
+                <div className="dashboard-counter-content"><div className="dashboard-counter-count">{formatNumber(completedScans)}</div>{` ${title}`}</div>
             }
         </div>
     )
