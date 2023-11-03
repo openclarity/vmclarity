a78bbfab991f5902c640c7915b4849eb1bbec09c
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-11-03 12:32:17 +0100

    refactor(scans-count): create separate displays

--- ui/src/layout/Dashboard/CounterDisplay/index.js
+++ ui/src/layout/Dashboard/CounterDisplay/index.js
@@ -2,10 +2,13 @@ import React, {useMemo} from 'react';
 import { useFetch } from 'hooks';
 import { formatNumber } from 'utils/utils';
 
+import COLORS from "../../../utils/scss_variables.module.scss";
+import { APIS } from "../../../utils/systemConsts";
+
 import './counter-display.scss';
 
-const CounterDisplay = ({url, title, background}) => {
-    const [{data, error, loading}] = useFetch(url, {queryParams: {"$select": "state"}});
+export const ScanCounterDisplay = () => {
+    const [{data, error, loading}] = useFetch(APIS.SCANS, {queryParams: {"$select": "state"}});
 
     const completedScans = useMemo(() => {
         let ret = 0;
@@ -20,9 +23,23 @@ const CounterDisplay = ({url, title, background}) => {
     }, [data])
 
     return (
+        <div className="dashboard-counter" style={{background: COLORS["color-gradient-green"]}}>
+            {loading || error ? "" :
+                <div className="dashboard-counter-content">
+                    <div className="dashboard-counter-count">{formatNumber(completedScans)}</div> Completed scans
+                </div>
+            }
+        </div>
+    )
+}
+
+export const CounterDisplay = ({url, title, background}) => {
+    const [{data, error, loading}] = useFetch(url, {queryParams: {"$count": true, "$top": 1, "$select": "id"}});
+
+    return (
         <div className="dashboard-counter" style={{background}}>
             {loading || error ? "" :
-                <div className="dashboard-counter-content"><div className="dashboard-counter-count">{formatNumber(completedScans)}</div>{` ${title}`}</div>
+                <div className="dashboard-counter-content"><div className="dashboard-counter-count">{formatNumber(data.count)}</div>{` ${title}`}</div>
             }
         </div>
     )
