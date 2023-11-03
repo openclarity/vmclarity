0c3082479f6c3021ad9685cace39a5b9ece2d9a9
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-11-03 15:53:52 +0100

    refactor(scans-count): simplify logic

--- ui/src/layout/Dashboard/CounterDisplay/index.js
+++ ui/src/layout/Dashboard/CounterDisplay/index.js
@@ -1,32 +1,26 @@
 import React, {useMemo} from 'react';
-import { useFetch } from 'hooks';
-import { formatNumber } from 'utils/utils';
+import {useFetch} from 'hooks';
+import {formatNumber} from 'utils/utils';
 
 import COLORS from "../../../utils/scss_variables.module.scss";
-import { APIS } from "../../../utils/systemConsts";
+import {APIS} from "../../../utils/systemConsts";
 
 import './counter-display.scss';
 
 export const ScanCounterDisplay = () => {
     const [{data, error, loading}] = useFetch(APIS.SCANS, {queryParams: {"$select": "state"}});
 
-    const completedScans = useMemo(() => {
-        let ret = 0;
-
-        data?.items.forEach((element) => {
-            if (element.state === "Aborted" || element.state === "Failed" || element.state === "Done") {
-                ret++
-            }
-        })
-
-        return ret
-    }, [data])
+    const completedScans = useMemo(
+        () => data?.items.filter((item) => ['Aborted', 'Failed', 'Done'].includes(item.state)).length ?? 0,
+        [data]
+    )
 
     return (
         <div className="dashboard-counter" style={{background: COLORS["color-gradient-green"]}}>
             {loading || error ? "" :
                 <div className="dashboard-counter-content">
-                    <div className="dashboard-counter-count">{formatNumber(completedScans)}</div> Completed scans
+                    <div className="dashboard-counter-count">{formatNumber(completedScans)}</div>
+                    Completed scans
                 </div>
             }
         </div>
@@ -39,7 +33,9 @@ export const CounterDisplay = ({url, title, background}) => {
     return (
         <div className="dashboard-counter" style={{background}}>
             {loading || error ? "" :
-                <div className="dashboard-counter-content"><div className="dashboard-counter-count">{formatNumber(data.count)}</div>{` ${title}`}</div>
+                <div className="dashboard-counter-content">
+                    <div className="dashboard-counter-count">{formatNumber(data.count)}</div>
+                    {` ${title}`}</div>
             }
         </div>
     )
