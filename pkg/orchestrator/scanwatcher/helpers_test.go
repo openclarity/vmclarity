1f79fd04ae3638b4bf6e2eb5f872ef77236a0cae
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-12-06 12:50:19 +0100

    test(api): fix unit test

--- pkg/orchestrator/scanwatcher/helpers_test.go
+++ pkg/orchestrator/scanwatcher/helpers_test.go
@@ -33,6 +33,13 @@ func TestNewAssetScanFromScan(t *testing.T) {
 
 	transitionTime := time.Now()
 
+	status := models.NewAssetScanStatus(
+		models.AssetScanStatusStatePending,
+		models.AssetScanStatusReasonCreated,
+		nil,
+	)
+	status.LastTransitionTime = transitionTime
+
 	resourceCleanupStatus := models.NewResourceCleanupStatus(
 		models.ResourceCleanupStatusStatePending,
 		models.ResourceCleanupStatusReasonAssetScanCreated,
@@ -141,11 +148,7 @@ func TestNewAssetScanFromScan(t *testing.T) {
 				Scan: &models.ScanRelationship{
 					Id: scanID,
 				},
-				Status: models.NewAssetScanStatus(
-					models.AssetScanStatusStatePending,
-					models.AssetScanStatusReasonCreated,
-					nil,
-				),
+				Status: status,
 				Sbom: &models.SbomScan{
 					Packages: nil,
 					Status:   sbomScanStatus,
@@ -215,6 +218,7 @@ func TestNewAssetScanFromScan(t *testing.T) {
 			g := NewGomegaWithT(t)
 
 			result, err := newAssetScanFromScan(test.Scan, test.AssetID)
+			result.Status.LastTransitionTime = transitionTime
 			result.ResourceCleanupStatus.LastTransitionTime = transitionTime
 			result.Sbom.Status.LastTransitionTime = transitionTime
 			result.Exploits.Status.LastTransitionTime = transitionTime
