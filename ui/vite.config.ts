14fb82f2ba6830f72c505bc283a4b5020985c238
Tamás Galló <tamasg@cisco.com>
2023-12-11 14:26:34 +0100

    feat(ui): use vite in the query-builder branch (#1017)
    * Merge branch 'switch-to-vite' into query-builder-with-vite
    
    * fix(ui): make query builder work with vitejs
    
    * fix(ui): remove the components that were moved
    
    * refactor(ui): import query builder Utils from mui

---
+++ ui/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { esbuildPluginBrowserslist } from 'esbuild-plugin-browserslist';
+import { browserslist } from './package.json';
+
+const PROXY_TARGET = 'http://localhost:8080';
+
+export default defineConfig(({ mode }) => {
+    return {
+        base: '',
+        plugins: [
+            react(),
+            viteTsconfigPaths(),
+            esbuildPluginBrowserslist(mode === 'production' ? browserslist.production : browserslist.development),
+        ],
+        build: {
+            outDir: 'build',
+        },
+        resolve: {
+            alias: {
+                utils: '/src/utils',
+                process: 'process/browser',
+            }
+        },
+        server: {    
+            open: true,
+            port: 3000,
+            proxy: {
+                '/api': {
+                    target: PROXY_TARGET,
+                },
+                '/ui/api': {
+                    target: PROXY_TARGET,
+                },
+            },
+        },
+    }
+});
