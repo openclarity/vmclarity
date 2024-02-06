4de1b80a47852bafbb30e389a7d6512bf8fbfb52
Catarina Paralta <46568597+paralta@users.noreply.github.com>
2024-02-06 15:15:14 +0000

    refactor(orchestrator): remove pkg folder from orchestrator and provider modules

--- orchestrator/cmd/main.go
+++ orchestrator/cmd/main.go
@@ -25,7 +25,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	orchestrator "github.com/openclarity/vmclarity/orchestrator/pkg"
+	"github.com/openclarity/vmclarity/orchestrator"
 	"github.com/openclarity/vmclarity/utils/log"
 	"github.com/openclarity/vmclarity/utils/version"
 )
