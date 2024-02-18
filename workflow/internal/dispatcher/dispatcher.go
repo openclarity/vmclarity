81eb74d2bc35f514b921d6cec206ed6163b63584
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2024-02-18 18:51:11 +0100

    feat: add workflow library
    Add initial version of the workflow manager which allows to run group of
    tasks operating a shared state in the right order based on hteir
    dependencies.

---
+++ workflow/internal/dispatcher/dispatcher.go
@@ -0,0 +1,183 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dispatcher
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/openclarity/vmclarity/workflow/types"
+)
+
+type Dispatcher[S any, R types.Runnable[S]] struct {
+	m  map[string]TaskStatus
+	mu *sync.RWMutex
+
+	stats *Stats
+
+	exitChan chan struct{}
+	nextChan chan struct{}
+}
+
+func (d *Dispatcher[S, R]) get(id string) (TaskStatus, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	s, ok := d.m[id]
+	if !ok {
+		return TaskStatus{}, fmt.Errorf("unknown task with ID: %s", id)
+	}
+
+	return s, nil
+}
+
+func (d *Dispatcher[S, R]) set(status *TaskStatus) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.m[status.id] = *status
+	d.stats.Update(status)
+}
+
+func (d *Dispatcher[S, R]) update(ctx context.Context, status TaskStatus) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		d.set(&status)
+		if status.Finished() {
+			select {
+			case <-d.exitChan:
+				return
+			case d.nextChan <- struct{}{}:
+			default:
+				return
+			}
+		}
+	}
+}
+
+func (d *Dispatcher[S, R]) stop() {
+	select {
+	case d.exitChan <- struct{}{}:
+	default:
+	}
+
+	close(d.exitChan)
+	close(d.nextChan)
+}
+
+func (d *Dispatcher[S, R]) Next() <-chan struct{} {
+	return d.nextChan
+}
+
+func (d *Dispatcher[S, R]) Dispatch(ctx context.Context, task R, state S) (bool, error) {
+	id := task.ID()
+
+	taskStatus, err := d.get(id)
+	if err != nil {
+		return false, fmt.Errorf("failed to get status for task with ID: %s", id)
+	}
+
+	if taskStatus.State != PENDING {
+		return true, nil
+	}
+
+	ok := true
+	for _, depID := range task.Dependencies() {
+		depStatus, err := d.get(depID)
+		if err != nil {
+			return false, fmt.Errorf("failed to get status for task with ID: %s", id)
+		}
+		if !depStatus.Finished() {
+			ok = false
+		}
+	}
+
+	if !ok {
+		return false, nil
+	}
+
+	taskStatus.State = RUNNING
+	d.update(ctx, taskStatus)
+
+	go func() {
+		err = task.Run(ctx, state)
+		d.update(ctx, taskStatus.FromError(err))
+	}()
+
+	return true, nil
+}
+
+func (d *Dispatcher[S, R]) Finished() bool {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	if d.stats.failed > 0 || d.stats.InProgress() == 0 {
+		return true
+	}
+
+	return false
+}
+
+func (d *Dispatcher[S, R]) Result() error {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	errs := make([]error, 0, len(d.m))
+	for _, status := range d.m {
+		errs = append(errs, status.Err)
+	}
+
+	return errors.Join(errs...)
+}
+
+func (d *Dispatcher[S, R]) String() string {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	var s string
+	for id, state := range d.m {
+		s += fmt.Sprintf("ID: %s, %s\n", id, state.String())
+	}
+
+	return s
+}
+
+func New[S any, R types.Runnable[S]](order []string) (*Dispatcher[S, R], func()) {
+	m := make(map[string]TaskStatus, len(order))
+	stats := &Stats{}
+
+	for _, id := range order {
+		status := TaskStatus{
+			id: id,
+		}
+		m[id] = status
+		stats.Update(&status)
+	}
+
+	d := &Dispatcher[S, R]{
+		m:        m,
+		mu:       &sync.RWMutex{},
+		stats:    stats,
+		exitChan: make(chan struct{}),
+		nextChan: make(chan struct{}, 1),
+	}
+
+	return d, d.stop
+}
