ff536693072aa6eb7f247df40b0422da1f879b71
Catarina Paralta <46568597+paralta@users.noreply.github.com>
2024-03-11 18:37:05 +0000

    feat(aws): add test asset

--- testenv/aws/asset/asset.go
+++ testenv/aws/asset/asset.go
@@ -15,27 +15,76 @@
 
 package asset
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+const (
+	DefaultAmazonMachineImage = "ami-03484a09b43a06725"
+	DefaultInstanceType       = ec2types.InstanceTypeT2Micro
+)
+
 // Create a new asset to be scanned in the test environment.
 // This asset will be an EC2 instance tagged with {"scanconfig": "test"}.
-// The asset will be created outside the VMClarity Server region.
 type Asset struct {
-	InstanceID  string
-	InstanceTag string
+	InstanceID string
 }
 
-func NewAsset(id, instanceID, instanceIP, instanceTag string) *Asset {
-	return &Asset{
-		InstanceID:  instanceID,
-		InstanceTag: instanceTag,
-	}
-}
-
-func (a *Asset) Create() error {
+func (a *Asset) Create(ctx context.Context, ec2Client *ec2.Client) error {
 	// Create an EC2 instance
+	ec2Output, err := ec2Client.RunInstances(
+		ctx,
+		&ec2.RunInstancesInput{
+			ImageId:      aws.String(DefaultAmazonMachineImage),
+			InstanceType: DefaultInstanceType,
+			MinCount:     aws.Int32(1),
+			MaxCount:     aws.Int32(1),
+		})
+	if err != nil {
+		return fmt.Errorf("failed to create test instance: %w", err)
+	}
+
+	a.InstanceID = *ec2Output.Instances[0].InstanceId
+
+	// Add tag to the created instance
+	_, err = ec2Client.CreateTags(
+		ctx,
+		&ec2.CreateTagsInput{
+			Resources: []string{a.InstanceID},
+			Tags: []ec2types.Tag{
+				{
+					Key:   aws.String("scanconfig"),
+					Value: aws.String("test"),
+				},
+			},
+		})
+	if err != nil {
+		return fmt.Errorf("failed to create tags for instance: %w", err)
+	}
+
 	return nil
 }
 
-func (a *Asset) Delete() error {
+func (a *Asset) Delete(ctx context.Context, ec2Client *ec2.Client) error {
+	if a.InstanceID == "" {
+		return nil
+	}
+
 	// Delete the EC2 instance
+	_, err := ec2Client.TerminateInstances(
+		ctx,
+		&ec2.TerminateInstancesInput{
+			InstanceIds: []string{a.InstanceID},
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to delete test instance: %w", err)
+	}
+
 	return nil
 }
