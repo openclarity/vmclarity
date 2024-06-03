eac6752752b59afc41d1dd711ed56ce9b036f836
Catarina Paralta <46568597+paralta@users.noreply.github.com>
2024-06-03 13:46:23 +0100

    feat(ui): remove asset details from findings

--- ui/src/layout/Findings/utils.jsx
+++ ui/src/layout/Findings/utils.jsx
@@ -1,44 +1,15 @@
 import TitleValueDisplay, { TitleValueDisplayRow } from 'components/TitleValueDisplay';
-import { formatDate, getAssetName } from 'utils/utils';
+import { formatDate } from 'utils/utils';
 
-const NAME_SORT_IDS = [
-    "asset.assetInfo.instanceID",
-    "asset.assetInfo.podName",
-    "asset.assetInfo.dirName",
-    "asset.assetInfo.imageID",
-    "asset.assetInfo.containerName"
-];
 
-export const getAssetAndScanColumnsConfigList = () => ([
-    {
-        Header: "Asset name",
-        id: "assetName",
-        sortIds: NAME_SORT_IDS,
-        accessor: (data) => getAssetName(data.asset.assetInfo),
-    },
-    {
-        Header: "Asset location",
-        id: "assetLocation",
-        sortIds: ["asset.assetInfo.location"],
-        accessor: (data) => data.asset.assetInfo.location || data.asset.assetInfo.repoDigests?.[0],
-    },
-    {
-        Header: "Found on",
-        id: "foundOn",
-        sortIds: ["foundOn"],
-        accessor: original => formatDate(original.foundOn)
-    }
-]);
-
-export const FindingsDetailsCommonFields = ({foundOn, invalidatedOn}) => (
+export const getScanColumnsConfigList = () => ([]);
+export const FindingsDetailsCommonFields = ({firstSeen, lastSeen}) => (
     <>
         <TitleValueDisplayRow>
-            <TitleValueDisplay title="Found on">{formatDate(foundOn)}</TitleValueDisplay>
+            <TitleValueDisplay title="First seen">{formatDate(firstSeen)}</TitleValueDisplay>
         </TitleValueDisplayRow>
-        {!!invalidatedOn &&
-            <TitleValueDisplayRow>
-                <TitleValueDisplay title="Invalidated on">{formatDate(invalidatedOn)}</TitleValueDisplay>
-            </TitleValueDisplayRow>
-        }
+        <TitleValueDisplayRow>
+            <TitleValueDisplay title="Last seen">{formatDate(lastSeen)}</TitleValueDisplay>
+        </TitleValueDisplayRow>
     </>
 )
