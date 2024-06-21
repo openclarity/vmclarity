a512d344382e32e76f959a7e915b0e5925254bde
András Jáky <ajaky@cisco.com>
2024-06-21 22:48:25 +0200

    feat(ui): fetch assets for finding on Finding page
    Signed-off-by: András Jáky <ajaky@cisco.com>

--- ui/src/layout/Findings/Secrets/SecretDetails.jsx
+++ ui/src/layout/Findings/Secrets/SecretDetails.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useFetch } from 'hooks';
 import TabbedPage from 'components/TabbedPage';
 import FindingsDetailsPage from '../FindingsDetailsPage';
 import TabSecretDetails from './TabSecretDetails';
+import Loader from 'components/Loader';
+import { APIS } from 'utils/systemConsts';
 
 const SECRET_DETAILS_PATHS = {
     SECRET_DETAILS: "",
@@ -10,9 +13,23 @@ const SECRET_DETAILS_PATHS = {
 
 const DetailsContent = ({data}) => {
     const {pathname} = useLocation();
-    
+
     const {id} = data;
-    
+
+    const filter = `finding/id eq '${id}'`;
+
+    const [{loading, data: assetFindingData, error}] = useFetch(APIS.ASSET_FINDINGS, {
+        queryParams: {"$filter": filter, "$count": true, "$expand": "asset"}
+    });
+
+    if (error) {
+        return null;
+    }
+
+    if (loading) {
+        return <Loader large />
+    }
+
     return (
         <TabbedPage
             basePath={`${pathname.substring(0, pathname.indexOf(id))}${id}`}
