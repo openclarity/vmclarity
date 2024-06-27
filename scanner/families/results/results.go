3b9a2ffbbaf7cb756d94a165e824067c892eea43
Ramiz Polic <ramiz.polic@hotmail.com>
2024-06-27 13:11:17 +0200

    feat: parallelize families

--- scanner/families/results/results.go
+++ scanner/families/results/results.go
@@ -17,10 +17,12 @@ package results
 
 import (
 	"errors"
+	"sync"
 )
 
-// Results store slice of results from all families.
+// Results store slice of results from all families. Safe for concurrent usage.
 type Results struct {
+	mu      sync.RWMutex
 	results []any
 }
 
@@ -29,17 +31,24 @@ func New() *Results {
 }
 
 func (r *Results) SetResults(result any) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	r.results = append(r.results, result)
 }
 
 // GetResult returns results for a specific family from the given results slice.
 func GetResult[familyType any](r *Results) (familyType, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
 	for _, result := range r.results {
 		res, ok := result.(familyType)
 		if ok {
 			return res, nil
 		}
 	}
+
 	var res familyType
 	return res, errors.New("missing result")
 }
