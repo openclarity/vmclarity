be910d1c07d0246632c56e22112d9350047488b6
Zsolt Kacsándi <zsolt@kacsandi.com>
2024-07-18 08:35:53 +0200

    refactor(scanner): replace use_clam_daemon option with use_native_clamscan

--- cli/cmd/scan/scan.go
+++ cli/cmd/scan/scan.go
@@ -179,9 +179,6 @@ func loadConfig(cfgFile string) *scanner.Config {
 	err := viper.ReadInConfig()
 	cobra.CheckErr(err)
 
-	// Set defaults
-	viper.SetDefault("malware.scanners_config.clam.use_clam_daemon", true)
-
 	// Load config
 	config := &scanner.Config{}
 	err = viper.Unmarshal(config)
